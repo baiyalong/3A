40b3b3424034c06fab038ac6654fc0c4bf0070bd
root <root@baiyalong.net>
2015-08-12 08:41:55 -0400

    add soap message support

--- server/rest.js
+++ server/rest.js
@@ -15,54 +15,76 @@ var api = new Restivus({
 });
 
 Products.find().forEach(function (e) {
-    var action = function () {
-        //console.log(this.request);
-        // console.log('-------------------------------------------------------------------------------------------')
-        var t0 = Date.now();
-        var res = false;
-        try {
-            //console.log(this.bodyParams);
-            //console.log('--------');
-            var result = HTTP.call(e.method, e.addr, {
-                npmRequestOptions: {rejectUnauthorized: false},
-                data: this.bodyParams
-            });
-            res = true;
-        } catch (err) {
-            // Got a network error, time-out or HTTP error in the 400 or 500 range.
-            console.error(err);
+    api.addRoute((function (e) {
+        var path = e.addr.substring(10);
+        path = path.substring(path.indexOf('/') + 1);
+        return path;
+    })(e), {authRequired: true}, (function (e) {
+        var endPoint = {};
+        endPoint[e.method.toLowerCase()] = {
+            roleRequired: e.roles,
+            action: function () {
+                var t0 = Date.now();
+
+                //1,get req message
+                var req = (function (req) {
+                    var body = '';
+                    body = (Meteor.wrapAsync(function (req, callback) {
+                        req.on('readable', function () {
+                            body += req.read();
+                            callback && callback(null, body);
+                        });
+                    }))(req);
+                    (Meteor.wrapAsync(function (req, callback) {
+                        req.on('end', function () {
+                            callback && callback(null);
+                        });
+                    }))(req);
+                    return {
+                        method: req.method,
+                        url: req.url,
+                        headers: req.headers,
+                        body: body
+                    }
+                })(this.request);
+
+                //2,make call
+                var resp = (function (e, req) {
+                    var res = {};
+                    try {
+                        res = HTTP.call(e.method, e.addr, {
+                            npmRequestOptions: {rejectUnauthorized: false},
+                            headers: req.headers,
+                            content: req.body
+                        });
+                    } catch (err) {
+                        console.error(err);
+                    }
+                    return res;
+                })(e, req);
+
+                //3,record
+                Records.insert({
+                    userid: this.userId,
+                    username: this.user.username,
+                    productid: e._id,
+                    name: e.name,
+                    addr: e.addr,
+                    method: e.method,
+                    req: JSON.stringify(req),
+                    resp: JSON.stringify(resp),
+                    timeStamp: Date(),
+                    timeInterval: Date.now() - t0
+                });
+
+                //4,return resp
+                return {
+                    statusCode: resp.statusCode,
+                    headers: resp.headers,
+                    body: resp.content
+                };
+            }
         }
-        var record = {
-            userid: this.userId,
-            username: this.user.username,
-            productid: e._id,
-            name: e.name,
-            addr: e.addr,
-            method: e.method,
-            req: 'req',
-            resp: 'resp',
-            timeStamp: Date(),
-            timeInterval: Date.now() - t0
-        };
-        Records.insert(record, function (err, res) {
-            if (err)console.error(err);
-            //  console.log(res);
-        });
-        // console.log('-------------------------------------------------------------------------------------------')
-        // console.log(this.response);
-        return {
-            statusCode: result.statusCode,
-            headers: result.headers,
-            body: result.data
-        }
-    };
-    var endPoint = {};
-    endPoint[e.method.toLowerCase()] = {
-        roleRequired: e.roles,
-        action: action
-    };
-    var path = e.addr.substring(10);
-    path = path.substring(path.indexOf('/'));
-    api.addRoute(path, {authRequired: true}, endPoint);
-})
-;
+        return endPoint;
+    })(e));
+});
